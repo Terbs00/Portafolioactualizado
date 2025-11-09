4070589bed8e304c1ac884519760997ac9a986f1
Terbs00 <sebasizaguirre36@gmail.com>
2025-11-08 20:15:07 -0600

    cambios  commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Portafolio/', 
+  base: '/Portafolioactualizado/', 
   build: {
     outDir: 'docs',   
     emptyOutDir: true
